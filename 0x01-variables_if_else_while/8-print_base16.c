8a5bc9be73db08eee9a983c6c47f3a2f4f3ed09a
Qedede <ededequaye@gmail.com>
2022-10-16 15:09:24 -0700

    8 fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,7 @@ int main(void)
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
-	for (lc = 'a'; lc <= 'f'; la++)
+	for (lc = 'a'; lc <= 'f'; lc++)
 		putchar(lc);
 
 	putchar('\n');
