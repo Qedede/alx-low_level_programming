422bc81d4a5d3233c52c112ce6289dbc24da98d7
Qedede <ededequaye@gmail.com>
2022-10-16 12:26:35 -0700

    8 fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Print all num of base 10
+ * main - Print all num of base 16
  * Return: Always 0
  */
 int main(void)
 {
 	int num;
+	char lc;
 
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
+
+	for (lc = 'a'; lc <= 'f'; la++)
+		putchar(lc);
 
 	putchar('\n');
 
