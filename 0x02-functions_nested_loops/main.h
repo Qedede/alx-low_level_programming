cd17752750306d101a05bc1bda55b01cdeec30af
Qedede <ededequaye@gmail.com>
2022-10-19 19:02:41 -0700

    Fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@ int _islower(int c);
 int _isalpha(int c);
 int _abs(int);
 int print_last_digit(int);
-void print_sign(int n);
+int print_sign(int n);
 void print_alphabet_x10(void);
 void jack_bauer(void);
 void times_table(void);
