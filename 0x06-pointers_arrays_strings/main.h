01913c9bb64d03b45e3abb0d40f4997ede343cb1
Qedede <ededequaye@gmail.com>
2022-10-27 18:30:03 -0700

    Another fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
 /**
  * void prototypes()
@@ -9,13 +8,13 @@
  */
 
 char *_strcat(char *dest, char *src);
-int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
+char *string_toupper(char *a);
+char *cap_string(char *a);
+char *leet(char *a);
+int _putchar(char c);                                               
 
 #endif /*MAIN_H*/
