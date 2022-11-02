4e4f2868da019bac0fb2553f0c18486c6c24ca9d
Qedede <ededequaye@gmail.com>
2022-11-01 19:00:58 -0700

    Getting quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * printf - prints output
+ * Return: one
+ */
+
+int main()
+{
+	printf("and that piece of art is useful - Dora Korpar, 2015-10-19 \n");
+	return (1);
+}
