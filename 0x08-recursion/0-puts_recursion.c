13bb52ea6e1eb0de58cdaf413f77e0a8f0ee17db
Qedede <ededequaye@gmail.com>
2022-11-02 19:12:26 -0700

    Fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
-	if (*s)
-		_puts_recursion(s);
-	else
+	if (*s == '\0')
+	{
 		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
 }
