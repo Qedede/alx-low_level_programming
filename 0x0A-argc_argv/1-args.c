67ba99022e84f589756cc2b77f0909350fd4f733
Qedede <ededequaye@gmail.com>
2022-11-04 20:42:38 -0700

    Fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * Return: always zero
  */
 
-int main(int argc, char *argc[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
