52b59de860b53a6c6cc2c8d501199d915577de51
Qedede <ededequaye@gmail.com>
2022-11-22 20:14:06 -0800

    Fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,12 @@
 
 /**
  * printf - prints output
+ * \" allows you to print a double quote in a double quote
  * Return: one
  */
 
 int main()
 {
-	printf("and that piece of art is useful - Dora Korpar, 2015-10-19 \n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
 	return (1);
 }
